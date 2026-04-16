66815b6be423f4f0b0e502e72aaefda13bf88315
phormaggione <davidebernardo66@gmail.com>
2026-04-16 13:54:19 +0000

    zioperaaaaaaa

--- 10_homework2/src/ArrayComplex.java
+++ 10_homework2/src/ArrayComplex.java
@@ -1,30 +1,53 @@
+import java.util.Scanner;
+
 public class ArrayComplex {
     public static void main(String[] args) throws Exception {
-        Complex[] vector = new Complex[3];
+
+        Scanner lettore = new Scanner(System.in);
         
-    }
+        System.out.println("Inserisci la dimensione:");
+        int dim = lettore.nextInt();
 
-    public void InsertComplexFromTerminal(Complex[] array, int DIM) {
+        Complex[] vector = new Complex[dim];
+        InsertComplexFromTerminal(vector, dim);
 
-    }
+        lettore.close();
 
-    public void PrintAll(Complex[] array, int DIM) {
-
-    }
-
-    public void BubbleSort(Complex[] array, int DIM) {
+        PrintMenu();
 
     }
 
-    public boolean Search(Complex[] array, int DIM, Complex e, int[] pos) {
+    public static void InsertComplexFromTerminal(Complex[] array, int DIM) {
+        double tempRe;
+        double tempImm;
+        for (int i = 0; i < DIM; i++) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Inserire la parte reale del numero complesso n" +i);
+            tempRe = input.nextDouble();
+            System.out.println("Inserire la parte immaginaria del numero complesso n" +i);
+            tempImm = input.nextDouble();
+            array[i] = new Complex(tempRe,tempImm);
+            input.close();
+        }
+    }
+
+            public static void PrintAll(Complex[] array, int DIM) {
+
+    }
+
+            public static void BubbleSort(Complex[] array, int DIM) {
+
+    }
+
+            public static boolean Search(Complex[] array, int DIM, Complex e, int[] pos) {
         return false;
     }
 
-    public void PrintAllModules(Complex[] array, int DIM) {
+            public static void PrintAllModules(Complex[] array, int DIM) {
 
     }
 
-    public void PrintMenu() {
+            public static void PrintMenu() {
         
     }
 
